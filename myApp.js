cbae54ab7196c279ca2ff604d39d7330fb1cfbb0
nourinawadd <nourinawad@gmail.com>
2025-04-24 22:44:43 +0200

    ch3

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require('express');
+require('dotenv').config();
 let app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-
+app.post("/name", function(req, res) {
+    // Handle the data in the request
+    var string = req.body.first + " " + req.body.last;
+    res.json({ name: string });
+  });
 
 
 
